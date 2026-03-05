092666a6fa70407c1f8330c8cb9f78822606dc6c
Iman Kebour <kebouriman6@gmail.com>
2026-03-05 13:32:11 +0100

    Refactor app.py for better organization and clarity
    Refactor application structure and database initialization.

--- planify/app.py
+++ planify/app.py
@@ -501,19 +501,15 @@ def logout():
     session.clear()
     return redirect(url_for("home"))
 
-# ---------------- INICIALIZAR DB Y ADMIN ----------------
-init_db()
-crear_admin_inicial()
 
-# ---------------- RUN APP ----------------
 if __name__ == "__main__":
     # Asegurarse de que el directorio para la DB exista
     os.makedirs("/opt/render/data", exist_ok=True)
 
-    # Crear tablas y admin inicial
+    # Inicializar la DB y crear admin si no existe
     init_db()
     crear_admin_inicial()
 
-    # Ejecutar app en el host y puerto que Render requiere
+    # Ejecutar la app en host y puerto que Render requiere
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
 
